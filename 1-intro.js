570739ffe42ce3cd7a78bc54cffecd8abcfae452
toyihill <39468459+toyihill@users.noreply.github.com>
2021-04-24 21:32:12 +0530

    1st commit

---
+++ 1-intro.js
@@ -0,0 +1,11 @@
+const amount = 12;
+
+if(amount< 10){
+    console.log('small number');
+
+}
+else{
+    console.log('large or equal number');
+}
+
+console.log('node app function');
--- README.md
+++
@@ -1,1 +0,0 @@
-# temp-node-tut
